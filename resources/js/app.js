9226614279cb6ad45d425a9bac9741803d258068
Ilham Nur Hakim <ilhamnurhakim378@gmail.com>
2018-10-22 20:42:18 +0700

    #25 Tinggal Edit form

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -8,6 +7,62 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import VueRouter from 'vue-router';
+import {
+    Form,
+    HasError,
+    AlertError
+} from 'vform';
+import moment from 'moment';
+import VueProgressBar from 'vue-progressbar';
+import swal from 'sweetalert2';
+
+window.Form = Form;
+
+Vue.component(HasError.name, HasError);
+Vue.component(AlertError.name, AlertError);
+Vue.use(VueRouter);
+Vue.filter('upText',(text)=>{
+    // return text.toUpperCase();
+    return text.charAt(0).toUpperCase() + text.slice(1); 
+});
+Vue.filter('myDate',(created)=>{
+    return moment(created).format(' Do MMMM YYYY');
+})
+Vue.use(VueProgressBar, {
+    color: 'rgb(143, 255, 199)',
+    failedColor: 'red',
+    height: '5px'
+});
+
+window.swal = swal;
+const toast = swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000
+});
+window.toast = toast;
+window.Fire = new Vue();
+
+let routes = [{
+        path: '/dashboard',
+        component: require('./components/Dashboard.vue')
+    },
+    {
+        path: '/users',
+        component: require('./components/Users.vue')
+    },
+    {
+        path: '/profile',
+        component: require('./components/Profile.vue')
+    }
+]
+
+const router = new VueRouter({
+    routes, // short for `routes: routes`,
+    mode: 'history'
+})
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -18,5 +73,6 @@ window.Vue = require('vue');
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
---
+++ resources/js/components/Users.vue
@@ -0,0 +1,201 @@
+<template>
+    <div class="container">
+        <div class="row mt-5">
+            <!-- mt margin top  -->
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Users Table</h3>
+                        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#addNew"> Add
+                            New <i class="fas fa-user-plus fa-fw"></i></button>
+                        <div class="card-tools">
+                            <div class="input-group input-group-sm" style="width: 150px;">
+                                <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+
+                                <div class="input-group-append">
+                                    <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-hover">
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>E-mail</th>
+                                <th>Type</th>
+                                <th>Register At</th>
+                                <th>Action</th>
+                            </tr>
+                            <tr v-for="user in users" :key="user.id">
+                                <td>{{user.id}}</td>
+                                <td>{{user.name}}</td>
+                                <td>{{user.email}}</td>
+                                <td><span class="tag tag-success">{{user.type | upText}}</span></td>
+                                <td>{{user.created_at | myDate}}</td>
+                                <td>
+                                    <a href="">
+                                        <i class="fa fa-edit"></i>
+                                    </a>
+                                    /
+                                    <a href="#" @click="deleteUser(user.id)">
+                                        <i class="fa fa-trash red"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+        </div><!-- /.row -->
+
+        <!-- Modal -->
+        <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNew" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addNew">Tambah Baru</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form @submit.prevent="createUser">
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Name</label>
+                                <input v-model="form.name" name="name" type="text" class="form-control" id="name"
+                                    :class="{ 'is-invalid' : form.errors.has('name') }" placeholder="Enter name">
+
+                                <has-error :form="form" field="name"></has-error>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input v-model="form.email" id="email" name="email" type="email" class="form-control"
+                                    :class="{ 'is-invalid' : form.errors.has('email') }" placeholder="Enter email">
+
+                                <has-error :form="form" field="email"></has-error>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Password</label>
+                                <input v-model="form.password" id="password" name="password" type="password" class="form-control"
+                                    :class="{ 'is-invalid' : form.errors.has('password') }" placeholder="Enter password">
+
+                                <has-error :form="form" field="password"></has-error>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Bio</label>
+                                <textarea v-model="form.bio" id="bio" type="text" class="form-control" :class="{ 'is-invalid' : form.errors.has('bio') }"
+                                    placeholder="Enter short bio for user">
+                            </textarea>
+                                <has-error :form="form" field="bio"></has-error>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Type</label>
+                                <select name="type" v-model="form.type" id="type" class="form-control" :class="{'is-invalid' :form.errors.has('type')}">
+                                    <option value="">Select User role</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="user">User</option>
+                                    <option value="author">Author</option>
+                                </select>
+                                <has-error :form="form" field="type"></has-error>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                            <button type="submit" class="btn btn-primary">Tambah baru</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                users: {},
+                form: new Form({
+                    name: '',
+                    email: '',
+                    password: '',
+                    type: '',
+                    bio: '',
+                    photo: ''
+                })
+            }
+        },
+        methods: {
+            deleteUser(id) {
+                swal({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    type: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.value) { //kirim ke server
+                        this.form.delete('api/user/' + id)
+                            .then(() => {
+                                swal(
+                                    'Deleted!',
+                                    'Your file has been deleted.',
+                                    'success'
+                                )
+                                Fire.$emit('AfterCreate'); //langsung ke load data baru
+                            })
+                            .catch(() => {
+                                swal(
+                                    'Failed!',
+                                    'Tidak berhasil, sesuatu ada yang salah',
+                                    'warning'
+                                );
+                            });
+                    }
+                })
+            },
+            loadUser() {
+                axios.get('api/user').then(({
+                    data
+                }) => (this.users = data.data));
+            },
+            createUser() {
+                this.$Progress.start();
+
+                this.form.post('api/user')
+                    .then(() => { //jika sukses
+                        Fire.$emit('AfterCreate'); //langsung ke load data baru
+                        $('#addNew').modal('hide');
+
+                        toast({
+                            type: 'success',
+                            title: 'Users Create in successfully'
+                        });
+
+                        this.$Progress.finish();
+                    })
+                    .catch(() => { //jika tidak / error
+
+                    })
+            }
+        },
+        created() {
+            this.loadUser();
+            Fire.$on('AfterCreate', () => {
+                this.loadUser(); //dari sini langsung updated
+            });
+            // setInterval(() => {
+            //     this.loadUser()
+            // }, 3000);
+        }
+    }
+
+</script>
